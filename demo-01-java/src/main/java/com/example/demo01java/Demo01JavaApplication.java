87d52f95a2c52fbe9d803bebc501767c9c5c5397
Firenze8 <2825317989@qq.com>
2020-06-02 17:01:29 +0800

    Initial commit repository

---
+++ demo-01-java/src/main/java/com/example/demo01java/Demo01JavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo01java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo01JavaApplication.class, args);
+    }
+
+}
